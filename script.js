55c9c4030bbca539ee7ff278e3824e42a8f29ef9
elitemassagemx <elitemassagemx@gmail.com>
2024-09-16 20:55:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -386,6 +386,45 @@ function init() {
         });
     });
 
+    //GALERIA
+var shadow = '0 20px 50px rgba(0,34,45,0.5)';
+
+function styles(item_id, x, y, z , opacity, shadow){
+	$(item_id).css({
+		transform: 'translate3d('+ x +'px, ' + y + 'px, ' + z +'px) ',
+		opacity: opacity,
+		'box-shadow': shadow
+	});
+}
+
+$('#one').click(function(){
+	$('#one').addClass('focus');
+	$('#two').removeClass('focus');
+	$('#three').removeClass('focus');
+	styles('#first', 0, 0, 0, 1, shadow);
+	styles('#second', 70, -80, -50, 0.6, 'none');
+	styles('#third', 110, 80, -60, 0.1, 'none');
+}); 
+
+
+$('#two').click(function(){
+	$('#one').removeClass('focus');
+	$('#two').addClass('focus');
+	$('#three').removeClass('focus');
+	styles('#first', 110, 80, -60, 0.1, 'none');
+	styles('#second', 0, 0, 0, 1, shadow);
+	styles('#third', 70, -80, -50, 0.6, 'none');
+});
+$('#three').click(function(){
+	$('#one').removeClass('focus');
+	$('#two').removeClass('focus');
+	$('#three').addClass('focus');
+	styles('#first', 70, -80, -50, 0.6, 'none');
+	styles('#second', 110, 80, -60, 0.1, 'none');
+	styles('#third', 0, 0, 0, 1, shadow);
+});
+    
+    
     // Inicializar el menú acordeón
     const menuIcon = document.createElement('img');
     // Inicializar el menú acordeón (continuación)
@@ -419,5 +458,17 @@ function init() {
         });
     }
 }
-
-// No necesitamos el evento DOMContentLoaded aquí, ya que init() se llama después de cargar los datos
+// Llama a init() cuando el DOM esté listo
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://raw.githubusercontent.com/elitemassagemx/Home/main/data.json')
+        .then(response => response.json())
+        .then(data => {
+            state.services = data.services;
+            state.packages = data.services.paquetes;
+            init();
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+            Utils.showNotification('Error al cargar los datos. Por favor, intenta de nuevo más tarde.');
+        });
+});
