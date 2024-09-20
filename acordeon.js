a3158fcc5d48e66ccc7fedd18c1005903634bbba
elitemassagemx <elitemassagemx@gmail.com>
2024-09-20 14:20:49 -0600

    Update acordeon.js

--- acordeon.js
+++ acordeon.js
@@ -1,26 +1,30 @@
 $(function() {
-	var Accordion = function(el, multiple) {
-		this.el = el || {};
-		this.multiple = multiple || false;
+    var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
 
-		// Variables privadas
-		var links = this.el.find('.link');
-		// Evento
-		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-	}
+        var links = this.el.find('.link');
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    }
 
-	Accordion.prototype.dropdown = function(e) {
-		var $el = e.data.el;
-			$this = $(this),
-			$next = $this.next();
+    Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+            $this = $(this),
+            $next = $this.next();
 
-		$next.slideToggle();
-		$this.parent().toggleClass('open');
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
 
-		if (!e.data.multiple) {
-			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-		};
-	}	
+        if (!e.data.multiple) {
+            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        };
+    }   
 
-	var accordion = new Accordion($('#accordion'), false);
+    var accordion = new Accordion($('#accordion'), false);
+
+    // Manejo del botón de menú
+    $('#menu-toggle').click(function() {
+        $('.main-nav').toggleClass('active');
+        $('body').toggleClass('menu-open');
+    });
 });
